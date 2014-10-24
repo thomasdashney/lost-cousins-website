550c89ee0c2733840b49e0808e804a1593287d74
Thomas Dashney <tdashney@gmail.com>
2014-10-23 23:47:34 -0400

    shows can be unnanounced now

--- js/main.js
+++ js/main.js
@@ -159,7 +159,7 @@ function loadShowData(cb) {
       yesterday.setDate(today.getDate()-1);
       for (var id in shows) {
         var show = shows[id];
-        if (show.date.obj < yesterday)
+        if (show.date.obj < yesterday || (show.announced && show.announced === 'false'))
           delete shows[id]; // delete the show
       }
       shows = _.values(shows); // get an array of shows (no ids)
