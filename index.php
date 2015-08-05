5424a0702b20392631b0a70f5c32f30e034f96f8
Thomas Dashney <tdashney@gmail.com>
2015-08-05 15:14:57 -0400

    added "feel an emotion" acoustic session video

--- index.php
+++ index.php
@@ -175,8 +175,8 @@
                         </a>
                     </h2>
                     <div id="videos">
+                        <iframe src="https://player.vimeo.com/video/135092427" width="640" height="390" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                         <iframe src="https://player.vimeo.com/video/126845367" width="640" height="390" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-                        <iframe src="https://player.vimeo.com/video/127674282" width="640" height="390" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                     </div>
                 </div>
             </div><!-- end media -->
